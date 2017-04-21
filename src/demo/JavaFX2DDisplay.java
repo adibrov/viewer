1de8c04cb18355af2c4c923de8695b2668eddbf7
adibrov <alexander.dibroff@gmail.comgradle>
2017-04-21 14:12:40 +0200

    optimisation: kernel parameters set only once or upon change

--- src/demo/JavaFX2DDisplay.java
+++ src/demo/JavaFX2DDisplay.java
@@ -12,18 +12,17 @@ import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import net.imglib2.img.basictypeaccess.offheap.ShortOffHeapAccess;
 import net.imglib2.img.planar.OffHeapPlanarImg;
 import net.imglib2.img.planar.OffHeapPlanarImgFactory;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 
-import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
 import static clearcontrol.simulation.loaders.SampleSpaceSaveAndLoad.loadUnsignedShortSampleSpaceFromDisk;
@@ -47,9 +46,10 @@ public class JavaFX2DDisplay extends Application {
         this.currentSlice = 0;
         this.mPlane = pPlane;
         int[] activeDims = mPlane.getActiveDims();
-        this.imgToDisplay = new DirectAccessImageByteGray((int)img.dimension(activeDims[0]), (int)img.dimension
+        this.imgToDisplay = new DirectAccessImageByteGray((int) img.dimension(activeDims[0]), (int) img.dimension
                 (activeDims[1]));
-        this.slicer = new Slicer(imgIn, imgToDisplay, Plane.XY);
+        System.out.println("image to display dims: " + imgToDisplay.getWidth() + " " + imgToDisplay.getHeight());
+        this.slicer = new Slicer(imgIn, imgToDisplay, mPlane);
         slicer.getSlice(currentSlice);
 
 
@@ -64,16 +64,17 @@ public class JavaFX2DDisplay extends Application {
         imageView_Source.setImage(imgToDisplay);
 
         VBox imageBox = new VBox(20);
-        imageBox.setPadding(new Insets(20,20,20,20));
+        imageBox.setPadding(new Insets(20, 20, 20, 20));
         imageBox.setMaxWidth(primaryStage.getMaxWidth());
         imageBox.setAlignment(Pos.TOP_CENTER);
 
 
-        imageView_Source.fitWidthProperty().bind(primaryStage.widthProperty().multiply(0.85));
-        imageView_Source.fitHeightProperty().bind(imageView_Source.fitWidthProperty());
+        imageView_Source.fitWidthProperty().bind(primaryStage.widthProperty().subtract(20));
+        imageView_Source.fitHeightProperty().bind(primaryStage.heightProperty().subtract(150));
+//        imageView_Source.fitHeightProperty().bind(imageView_Source.fitWidthProperty());
         VBox vBoxSliders = new VBox(15);
         vBoxSliders.setMinHeight(100);
-        vBoxSliders.setPadding(new Insets(20,20,20, 20));
+        vBoxSliders.setPadding(new Insets(20, 20, 20, 20));
         vBoxSliders.setAlignment(Pos.BOTTOM_CENTER);
 
         Slider sliderZ = new Slider(0, imgIn.dimension(mPlane.getSlideDim()), 0);
@@ -82,29 +83,27 @@ public class JavaFX2DDisplay extends Application {
             currentSlice = n.intValue();
             slicer.getSlice(currentSlice);
         });
-        Slider sliderMin = new Slider(0,511,0);
+        Slider sliderMin = new Slider(0, 511, 0);
         sliderMin.setBlockIncrement(1);
-        sliderMin.valueProperty().addListener((ob,o,n)->{
+        sliderMin.valueProperty().addListener((ob, o, n) -> {
             slicer.setMin(n.shortValue());
             slicer.convert();
         });
 
-        Slider sliderMax = new Slider(0,511,511);
+        Slider sliderMax = new Slider(0, 511, 511);
         sliderMax.setBlockIncrement(1);
-        sliderMax.valueProperty().addListener((ob,o,n)->{
+        sliderMax.valueProperty().addListener((ob, o, n) -> {
             slicer.setMax(n.shortValue());
             slicer.convert();
         });
 
         vBoxSliders.getChildren().addAll(sliderZ, sliderMin, sliderMax);
-        imageBox.getChildren().addAll(imageView_Source,vBoxSliders);
-
+        imageBox.getChildren().addAll(imageView_Source, vBoxSliders);
 
 
         StackPane root = new StackPane();
         root.getChildren().add(imageBox);
-        Scene scene = new Scene(root, 500, 600);
-
+        Scene scene = new Scene(root, 500, imgToDisplay.getHeight() + 150);
 
 
         primaryStage.setTitle("Current slice");
@@ -114,15 +113,15 @@ public class JavaFX2DDisplay extends Application {
 
     }
 
-    private void notifyListeners() {
-        for (Listener listener : listenerList) {
-            listener.fire();
-        }
-    }
-
-    public void addListener(Listener listener) {
-        listenerList.add(listener);
-    }
+//    private void notifyListeners() {
+//        for (Listener listener : listenerList) {
+//            listener.fire();
+//        }
+//    }
+//
+//    public void addListener(Listener listener) {
+//        listenerList.add(listener);
+//    }
 
     public void run(String[] args) {
         launch(args);
@@ -137,17 +136,10 @@ public class JavaFX2DDisplay extends Application {
         short[] rawArr = new short[dims[0] * dims[1] * dims[2]];
         rawArr = loadUnsignedShortSampleSpaceFromDisk(path, dims[0], dims[1], dims[2]);
 
-        System.out.println("length:" + rawArr.length);
-
-        for (int i = 0; i < 100; i++) {
-            System.out.println(rawArr[i]);
-        }
-
         Object h = new Object();
         OffHeapMemory cmIn = null;
         OffHeapMemory cmOut = null;
         cmIn = new OffHeapMemory("mem", h, OffHeapMemoryAccess.allocateMemory((2 * rawArr.length)), 2 * rawArr.length);
-        cmOut = new OffHeapMemory("mem", cmOut, OffHeapMemoryAccess.allocateMemory((rawArr.length)), rawArr.length);
         cmIn.copyFrom(rawArr);
 
 
@@ -156,29 +148,12 @@ public class JavaFX2DDisplay extends Application {
                 UnsignedShortType
                 ());
 
-
-//        System.out.println("image:");
-//        for (int i = 0; i < 100; i++) {
-//            System.out.println(cmIn.getByteBuffer().get(i));
-//        }
-
-
-        DirectAccessImageByteGray img = new DirectAccessImageByteGray(dims[0], dims[1]);
-
-
-
         JavaFX2DDisplay display = new JavaFX2DDisplay(imgIn, Plane.XY);
-//        display.addListener(() -> {
-//            slicer.getSlice(display.currentSlice);
-//        });
 
         new JFXPanel();
         Platform.runLater(() -> {
             Stage s = new Stage();
             display.start(s);
         });
-
-
     }
-
 }
--- src/image/Slicer.java
+++ src/image/Slicer.java
@@ -39,30 +39,23 @@ public class Slicer {
     private cl_mem interm;
     private cl_mem outputImgCL;
 
+    private long[] workSizeSlice;
+    private long[] workSizeConvert;
+
 
     public Slicer(OffHeapPlanarImg<UnsignedShortType, ShortOffHeapAccess> inputImg,
                   DirectAccessImageByteGray img, Plane pPlane) {
         this.image = img;
+        this.mPlane = pPlane;
         this.sizeX = (int) inputImg.dimension(0);
         this.sizeY = (int) inputImg.dimension(1);
         this.sizeZ = (int) inputImg.dimension(2);
-        this.planeSizeX = (int) inputImg.dimension(0);
-        this.planeSizeY = (int) inputImg.dimension(1);
-        this.mPlane = pPlane;
+        int[] dimsInd = mPlane.getActiveDims();
+        this.planeSizeX = (int) inputImg.dimension(dimsInd[0]);
+        this.planeSizeY = (int) inputImg.dimension(dimsInd[1]);
+
         this.min = 0;
         this.max = 511;
-
-        boolean compat = false;
-
-        int[] dims = selectedPlaneSize(mPlane);
-        compat = (dims[0] == planeSizeX & dims[1] == planeSizeY);
-
-        if (!compat) {
-            throw new IllegalArgumentException(String.format("Wrong output img parameters: %s slice requires %dx%d, " +
-                    "provided: %dx%d", mPlane, dims[0], dims[1], planeSizeX, planeSizeY));
-        }
-
-//        System.out.println(String.format("img size: %dx%d", sizeX, sizeY));
 
         this.srcImg = inputImg;
         this.trgImg = trgImg;
@@ -70,49 +63,20 @@ public class Slicer {
         this.kernelSourceConvert = readFile("resources/kernels/SixteenToEightBit.cl");
 
         initCL();
-    }
 
-    public void setMin(short min) {
-        this.min = min;
-    }
-
-    public void setMax(short max) {
-        this.max = max;
-    }
-
-    private int[] selectedPlaneSize(Plane pPlane) {
-        int[] res = new int[2];
-        switch (mPlane) {
-            case XY:
-                res[0] = sizeX;
-                res[1] = sizeY;
-                break;
-            case XZ:
-                res[0] = sizeX;
-                res[1] = sizeZ;
-                break;
-            case YZ:
-                res[0] = sizeY;
-                res[1] = sizeZ;
-        }
-        return res;
-    }
-
-    public void getSlice(int slicePos) {
-
-        System.out.println("slicer pos: " + slicePos);
-        System.out.println("slicer dim: " + mPlane.getSlideDim());
-
-        long workSizeSlice[] = new long[3];
+        this.workSizeSlice = new long[3];
         workSizeSlice[0] = sizeX;
         workSizeSlice[1] = sizeY;
         workSizeSlice[2] = sizeZ;
-//        System.out.println(String.format("3d stack %dx%dx%d", sizeX, sizeY, sizeZ));
 
+        this.workSizeConvert = new long[2];
+        workSizeConvert[0] = planeSizeX;
+        workSizeConvert[1] = planeSizeY;
 
         clEnqueueWriteBuffer(commandQueue, pixelMem, true, 0,
                 Sizeof.cl_short * sizeY * sizeX*sizeZ, Pointer.to(srcImg.getContiguousMemory().getByteBuffer()), 0,
                 null, null);
+
 
         clSetKernelArg(kernelSlice, 0, Sizeof.cl_mem, Pointer.to(pixelMem));
         clSetKernelArg(kernelSlice, 1, Sizeof.cl_mem, Pointer.to(interm));
@@ -120,38 +84,6 @@ public class Slicer {
         clSetKernelArg(kernelSlice, 3, Sizeof.cl_int, Pointer.to(new int[]{sizeY}));
         clSetKernelArg(kernelSlice, 4, Sizeof.cl_int, Pointer.to(new int[]{sizeZ}));
         clSetKernelArg(kernelSlice, 5, Sizeof.cl_int, Pointer.to(new int[]{mPlane.getSlideDim()}));
-        clSetKernelArg(kernelSlice, 6, Sizeof.cl_int, Pointer.to(new int[]{slicePos}));
-
-
-        clEnqueueNDRangeKernel(commandQueue, kernelSlice, 3, null,
-                workSizeSlice, null, 0, null, null);
-
-
-        convert();
-
-
-
-//        ShortBuffer sh = ShortBuffer.allocate(planeSizeX*planeSizeY);
-//        clEnqueueReadBuffer(commandQueue, interm, CL_TRUE, 0,
-//                Sizeof.cl_short * planeSizeX * planeSizeY, Pointer.to(sh), 0, null,
-//                null);
-
-//        System.out.println("what we've got:");
-//        for (int i = 0; i <500; i++) {
-//            System.out.println(sh.get(i));
-//        }
-
-//        this.image.getBuffer().rewind();
-
-
-    }
-
-    public void convert() {
-
-        long workSizeConvert[] = new long[2];
-        workSizeConvert[0] = planeSizeX;
-        workSizeConvert[1] = planeSizeY;
-//        System.out.println(String.format("2d slice: %dx%d",planeSizeX, planeSizeY));
 
         clSetKernelArg(kernelConvert, 0, Sizeof.cl_mem, Pointer.to(interm));
         clSetKernelArg(kernelConvert, 1, Sizeof.cl_mem, Pointer.to(outputImgCL));
@@ -159,16 +91,36 @@ public class Slicer {
         clSetKernelArg(kernelConvert, 3, Sizeof.cl_int, Pointer.to(new int[] {planeSizeY}));
         clSetKernelArg(kernelConvert, 4, Sizeof.cl_short, Pointer.to(new short[] {this.min}));
         clSetKernelArg(kernelConvert, 5, Sizeof.cl_short, Pointer.to(new short[] {this.max}));
+    }
 
+    public void setMin(short min) {
+        this.min = min;
+        clSetKernelArg(kernelConvert, 4, Sizeof.cl_short, Pointer.to(new short[] {this.min}));
+
+    }
+
+    public void setMax(short max) {
+        this.max = max;
+        clSetKernelArg(kernelConvert, 5, Sizeof.cl_short, Pointer.to(new short[] {this.max}));
+
+    }
+
+
+    public void getSlice(int slicePos) {
+        clSetKernelArg(kernelSlice, 6, Sizeof.cl_int, Pointer.to(new int[]{slicePos}));
+        clEnqueueNDRangeKernel(commandQueue, kernelSlice, 3, null,
+                workSizeSlice, null, 0, null, null);
+        convert();
+    }
+
+    public void convert() {
         clEnqueueNDRangeKernel(commandQueue, kernelConvert, 2, null,
                 workSizeConvert, null, 0, null, null);
-
         clEnqueueReadBuffer(commandQueue, outputImgCL, CL_TRUE, 0,
                 Sizeof.cl_char * planeSizeX * planeSizeY, Pointer.to(this.image.getBuffer()), 0, null,
                 null);
         try {
             this.image.update();
-            System.out.println("upd image");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -271,12 +223,9 @@ public class Slicer {
 
     public static void main(String[] args) {
 
-
         Plane sth = Plane.XZ;
         String s = sth.toString();
         System.out.println(s);
-
-
     }
 
 }
